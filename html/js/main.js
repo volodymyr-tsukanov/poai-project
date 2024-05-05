3c535ca599424bb05e81d2103b2f5fbcd360ff7a
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-05-05 21:10:07 +0200

    updated pages (settings, main) ; updated style (nav animation)

--- html/js/main.js
+++ html/js/main.js
@@ -1,11 +1,7 @@
 class Settings {
 	constructor(){
 		let s = JSON.parse(localStorage.getItem('settings'));
-		if(s == null || s == undefined){
-			this.lastPage = -1; //load settings page by default
-			this.lang = 'en';
-			this.save();
-		}
+		if(s == null || s == undefined) this.reset();
 		else{
 			this.lang = s.lang;
 			this.lastPage = s.lastPage;
@@ -17,6 +13,11 @@ class Settings {
 		if(lang != undefined) this.lang = lang;
 		this.save();
 	}
+	reset(){
+		this.lastPage = -1; //load settings page by default
+		this.lang = 'en';
+		this.save();
+	}
 
 	save(){
 		const jsonData = { lang : this.lang, lastPage : this.lastPage };
@@ -24,9 +25,12 @@ class Settings {
 	}
 }
 
+
+//Preload
+var settings = new Settings();
+
 document.addEventListener('DOMContentLoaded', () => {
-	// Preload
-	var settings = new Settings();
+	const container = document.getElementById('cntnr');
 
 	// Awake
 	loadPage(settings.lastPage, settings.lang);
@@ -53,49 +57,50 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 function loadPage(pageId, lang) {
-	var settings = new Settings();
-	if(lang == undefined) lang = settings.lang;
-	const navigation = document.getElementById('navBtns');
 	const mainBody = document.getElementById('mainBody');
+
+	if(lang == undefined) lang = settings.lang;
 
 	switch(pageId){
 		case -1: //settings
-			mainBody.innerHTML = 'settings';
-			fetch('blocks/settings.html')
-			.then(response => {
-				if (!response.ok) {
-					throw new Error(`HTTP error ${response.status}`);
-				}
-				return response.text();
-			}).then(html => {
-				mainBody.innerHTML = html;
-			}).catch(e => {
-				console.error(e);
+			fetchPage('blocks/settings.html', mainBody).then(() => {
+				const nav = document.querySelector('nav');
+				nav.style = 'display:none';
 			});
 			break;
 		case 0: //main
-			mainBody.innerHTML = 'main';
+			fetchPage('blocks/main.html', mainBody);
 			break;
 		case 1: //projects
-			mainBody.innerHTML = 'projects';
+			fetchPage('blocks/projects.html', mainBody);
 			break;
 		case 2: //forms
-			mainBody.innerHTML = 'forms';
+			fetchPage('blocks/forms.html', mainBody);
 			break;
 		case 3: //contacts
-			mainBody.innerHTML = 'contacts';
+			fetchPage('blocks/contacts.html', mainBody);
 			break;
 		default:
-			mainBody.innerHTML = '! No such page !';
 			break;
 	}
 
 	settings.set(pageId, lang);
 	console.log(pageId, lang);
 }
+async function fetchPage(path, element){
+	try {
+		const response = await fetch(path);
+		if (!response.ok) {
+			throw new Error(`HTTP error ${response.status}`);
+		}
+		const html = await response.text();
+		element.innerHTML = html;
+	} catch (e) {
+		console.error(e);
+	}
+}
 
 function saveSettings(){
-	var settings = new Settings();
 	let lang = 'en';
 	let sl = document.getElementsByName('settingsLangs');
 	for(let i = 0; i < sl.length; i++){
@@ -107,5 +112,5 @@ function saveSettings(){
 	settings.set(0, lang);
 }
 function resetSettings(){
-
+	settings.reset();
 }
