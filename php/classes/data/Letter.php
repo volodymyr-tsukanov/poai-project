9c88ef4335e800b6e7c0e0af2e3cfd27e809b535
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-01-10 14:24:02 +0100

    updated class Letter

--- php/classes/data/Letter.php
+++ php/classes/data/Letter.php
@@ -16,5 +16,96 @@
 */
 namespace project_VT\data;
 
+use DateTime;
 
+
+enum LetterStatus: int {
+    case New = 1;
+    case Read = 11;
+    case Answered = 111;
+    case Important = 122;
+}
+enum LetterSubject: int{
+    case ThisSite = 1;
+    case nspec = 0;
+}
+
+
+class Letter {
+    private int $id;
+    protected string $message;
+    protected DateTime $created;
+    protected LetterStatus $status;
+    protected LetterSubject $subject;
+
+
+    function __construct(string $message, DateTime $created, LetterStatus $status, LetterSubject $subject)
+    {
+        $this->message = $message;
+        $this->created = $created;
+        $this->status = $status;
+        $this->subject = $subject;
+    }
+    function __destruct(){
+        unset($this->date);
+    }
+
+    public static function fromStdClass(stdClass $object): Letter{
+        return new self($object->username, $object->email, $passwd, $object->language);
+    }
+    public static function fromJson(string $jsonData): Letter{
+        $json = json_decode($jsonData);
+        return new self($json->username, $json->email, $json->passwd, $json->language);
+    }
+
+    public function getId(): int{
+        return $this->id;
+    }
+    public function getMessage(): string{
+        return $this->message;
+    }
+    public function setMessage(string $message){
+        $this->message = $message;
+    }
+
+    public function getCreated(): DateTime{
+        return $this->created;
+    }
+    public function setCreated(DateTime $created){
+        $this->created = $created;
+    }
+
+    public function getStatus(): LetterStatus{
+        return $this->status;
+    }
+    public function setStatus(LetterStatus $status){
+        $this->status = $status;
+    }
+
+    public function getSubject(): LetterSubject{
+        return $this->subject;
+    }
+    public function setSubject(LetterSubject $subject){
+        $this->subject = $subject;
+    }
+
+
+    public function show(){
+        printf('User: %s %s status=%d %s', $this->username,$this->email,$this->status,Warden::packTime($this->updated));
+    }
+
+    public function toArray(): array {
+        return [
+            "username" => $this->username,
+            "email" => $this->email,
+            "passwd" => $this->passwd,
+            "updated" => Warden::packTime($this->updated),
+            "status" => (int)$this->status,
+            "language" => (int)$this->language
+        ];
+    }
+    public function toJSON(): string{
+        return json_encode($this->toArray());
+    }
+}
 ?>
