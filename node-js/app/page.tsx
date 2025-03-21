731589aaf1826bd24188bf18f84fdebbc53f4966
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-03-21 21:04:10 +0100

    restructurized project(removed redundant `app` folder)

--- node-js/app/page.tsx
+++ node-js/app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
 
+function PageTwo(props:{n:number}){
+  return (<div>
+    <Image
+      src="/next.svg"
+      alt="Next.js logo"
+      width={180}
+      height={30}
+    />
+    Page num {props.n}
+  </div>);
+}
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <PageTwo
+          n={9}
+        />
+
         <Image
           className="dark:invert"
           src="/next.svg"
